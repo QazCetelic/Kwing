953dcb30ba1a63701c780550825800ce60c1998f
Qaz Cetelic <51381523+QazCetelic@users.noreply.github.com>
2021-10-01 11:04:23 +0200

    Add default columns value

--- src/main/kotlin/kwing/components/fields/IntField.kt
+++ src/main/kotlin/kwing/components/fields/IntField.kt
@@ -1,6 +1,6 @@
 package kwing.components.fields
 
-open class IntField(int: Int, columns: Int): ValueField<Int>(int, columns) {
+open class IntField(int: Int, columns: Int = 20): ValueField<Int>(int, columns) {
     override val emptyFieldFallback: Int = 0
     override fun verifyInput(text: String): Boolean = text.toIntOrNull() != null
     var int: Int
